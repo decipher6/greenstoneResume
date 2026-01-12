928aaa144245a8dfbbb0bc6c2a8e2882a013308e
decipher6 <yg2810@nyu.edu>
2026-01-12 15:54:04 +0400

    reduce logo size frm 30x30 to 22x22

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -39,7 +39,7 @@ const Layout = ({ children, pageTitle, pageSubtitle }) => {
           <img 
             src="/logo.svg" 
             alt="Greenstone Logo" 
-            className="w-25 h-25 object-contain"
+            className="w-22 h-22 object-contain"
           />
           <div className="text-center">
             <p className="text-xs text-gray-400">Talent Management AI</p>
